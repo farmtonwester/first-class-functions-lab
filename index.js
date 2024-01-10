b60097779bcca1cc6b200120a94d5da9a5cf33cd
Weston Farmer <wesfarmer11@gmail.com>
2024-01-09 18:55:59 -0600

    all driver functions pass tests

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(names) {
+    return names.slice(0,2)
+};
+
+const returnLastTwoDrivers = function(names) {
+    return names.slice(-2)
+};
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer) {
+    return function(fare){
+        return (fare * integer)
+    }
+};
+const fareDoubler = createFareMultiplier(2) 
+    
+//console.log(fareDoubler(10))
+
+const fareTripler = createFareMultiplier(3)
+
+
+function selectDifferentDrivers(drivers, cb) {
+    return cb(drivers)
+};
